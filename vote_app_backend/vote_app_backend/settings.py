fdf166a17c536fdb056f4df941cc7bdeed515d51
JaninaAlona <jelona@posteo.net>
2022-04-14 14:25:02 +0200

    allowed host added to settings.py

--- vote_app_backend/vote_app_backend/settings.py
+++ vote_app_backend/vote_app_backend/settings.py
@@ -148,3 +148,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+ALLOWED_HOSTS = ['0.0.0.0']
