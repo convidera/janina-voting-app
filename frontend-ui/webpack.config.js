1fe509a25d470c0ec86c29dc7b499c906ec02323
JaninaAlona <jelona@posteo.net>
2022-05-12 14:32:24 +0200

    webpack config added

---
+++ frontend-ui/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    module: {
+        rules: [
+          {
+            test: /\.vue$/,
+            loader: 'vue-loader',
+            options: {
+              hotReload: false 
+            }
+          }
+        ]
+      }
+
+}
