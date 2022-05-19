ab502c2aa8ba258b2aca0c50d9fd8ad81b85f304
JaninaAlona <jelona@posteo.net>
2022-05-19 15:18:23 +0200

    expect scripts done

--- devops.sh
+++ devops.sh
@@ -3,6 +3,7 @@
 COMPOSE="docker-compose"
 
 
+
 if [ $# -gt 0 ]
 then
   if [ "$1" == "exit" ];then
@@ -51,7 +52,23 @@ then
         if [ -z "$GITHUB_USER" ] && [ -z "$GITHUB_TOKEN" ];then
           echo "environment variables unset in .env file"
         else
-          ./pullserverbot.exp
+          ./pullserverbot.exp $GITHUB_USER $GITHUB_TOKEN
+        fi
+      else
+        echo "environment variables missing in .env file"
+      fi
+    else
+      echo ".env file missing"
+    fi
+  elif [ "$1" == "sshserver" ];then
+    if [ -f .env ]; then
+      export $(cat .env | xargs)
+      if grep -Fq SERVER_USER .env && grep -Fq SERVER_IP .env && grep -Fq SERVER_PASSPHRASE .env
+      then
+        if [ -z "$SERVER_USER" ] && [ -z "$SERVER_IP" ] && [ -z "$SERVER_PASSPHRASE" ];then
+          echo "environment variables unset in .env file"
+        else
+          ./sshserverbot.exp $SERVER_USER $SERVER_IP $SERVER_PASSPHRASE
         fi
       else
         echo "environment variables missing in .env file"
