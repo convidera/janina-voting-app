297c2b482b79bd40e59881d933fa7541ae1098fa
JaninaAlona <jelona@posteo.net>
2022-06-03 16:03:47 +0200

    no connection between containers

--- .deploy/local/vue.config.js
+++ .deploy/local/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    host: '0.0.0.0',
+    host: 'janina.test',
     port: 80,
   },
 })
