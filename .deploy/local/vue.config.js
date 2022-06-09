b0bec667619d03cd007f40438028e5e008fc4e33
JaninaAlona <jelona@posteo.net>
2022-06-09 14:27:43 +0200

    connection to DB, loadbalancers correctly set up

--- .deploy/local/vue.config.js
+++ .deploy/local/vue.config.js
@@ -1,8 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+//const fs = require('fs')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    // https: {
+    //   key: fs.readFileSync('./certs/janina.test+3-key.pem'),
+    //   cert: fs.readFileSync('./certs/janina.test+3.pem'),
+    // },
+    // public: 'https://janina.test:443',
     host: '0.0.0.0',
-    port: 443,
+    port: 80,
+    //https: true,
   },
 })
