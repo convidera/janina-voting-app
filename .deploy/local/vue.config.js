5d92eb69ac14337dbfba1b9aa4906299f08c6a6a
JaninaAlona <jelona@posteo.net>
2022-06-09 16:28:40 +0200

    website accessible, but websocket blocks connection

--- .deploy/local/vue.config.js
+++ .deploy/local/vue.config.js
@@ -1,16 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
-//const fs = require('fs')
 
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    // https: {
-    //   key: fs.readFileSync('./certs/janina.test+3-key.pem'),
-    //   cert: fs.readFileSync('./certs/janina.test+3.pem'),
-    // },
-    // public: 'https://janina.test:443',
-    host: '0.0.0.0',
-    port: 80,
-    //https: true,
+    allowedHosts: [
+      'www.janina.test',
+      'janina.test',
+      'www.janina.test/api/',
+      'janina.test/api/',
+      'www.janina.test/api/get-csrf',
+      'janina.test/api/get-csrf',
+    ],
   },
 })
