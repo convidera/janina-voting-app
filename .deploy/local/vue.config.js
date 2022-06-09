a84b9bd16a3fe06fce4023ea80671bb5f350785f
JaninaAlona <jelona@posteo.net>
2022-06-09 17:29:34 +0200

    removing comments

--- .deploy/local/vue.config.js
+++ .deploy/local/vue.config.js
@@ -6,10 +6,6 @@ module.exports = defineConfig({
     allowedHosts: [
       'www.' + process.env.VUE_APP_DOMAIN_URL,
       process.env.VUE_APP_DOMAIN_URL,
-      // 'www.janina.test/api/',
-      // 'janina.test/api/',
-      // 'www.' + process.env.VUE_APP_DOMAIN_URL + '/' + process.env.VUE_APP_URI_ENTRYP_PATH + process.env.VUE_APP_URI_CSRF_PATH,
-      // process.env.VUE_APP_DOMAIN_URL + '/' + process.env.VUE_APP_URI_ENTRYP_PATH + process.env.VUE_APP_URI_CSRF_PATH,
     ],
   },
 })
