ce967cad66e0dadfc7a1eeab82f8c6f25a35f5c6
JaninaAlona <jelona@posteo.net>
2022-06-14 18:20:26 +0200

    vue config modified

--- .deploy/stage/vue.config.js
+++ .deploy/stage/vue.config.js
@@ -1,5 +1,16 @@
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+// });
+
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-});
-
+  devServer: {
+    allowedHosts: [
+      'www.' + process.env.VUE_APP_DOMAIN_URL,
+      process.env.VUE_APP_DOMAIN_URL,
+    ],
+  },
+})
