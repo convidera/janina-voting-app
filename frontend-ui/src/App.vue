6217b0776f984da94db26ed04f1c73d883571c02
JaninaAlona <jelona@posteo.net>
2022-06-22 12:52:28 +0200

    env test

--- frontend-ui/src/App.vue
+++ frontend-ui/src/App.vue
@@ -67,6 +67,7 @@ export default {
   },
   methods: {
     getCSRF() {
+      console.log(process.env.VUE_APP_DOMAIN_URL) 
       axios.get('https://' + process.env.VUE_APP_DOMAIN_URL + '/' + process.env.VUE_APP_URI_ENTRYP_PATH + process.env.VUE_APP_URI_CSRF_PATH)
       .then(response => (this.info = response))
     },
