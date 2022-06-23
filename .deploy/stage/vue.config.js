7dfd679d77a367dfbad624797b1cbb563d63ed31
JaninaAlona <jelona@posteo.net>
2022-06-23 08:46:01 +0200

    devserver vue configured

--- .deploy/stage/vue.config.js
+++ .deploy/stage/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  // devServer: {
-  //     proxy: 'https://' + process.env.VUE_APP_DOMAIN_URL,
-  //   }
+  devServer: {
+      proxy: 'https://' + process.env.VUE_APP_DOMAIN_URL,
+    }
 })
