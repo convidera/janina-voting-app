7980287c5a4bc973e64021e1cb9d1ceca38e7d5b
JaninaAlona <jelona@posteo.net>
2022-06-23 07:16:30 +0200

    ready to rebuild

--- vote_app_backend/manage.py
+++ vote_app_backend/manage.py
@@ -3,13 +3,12 @@
 import os
 import sys
 import dotenv
-#from dotenv import load_dotenv
 
 
 def main():
     """Run administrative tasks."""
     dotenv.read_dotenv()
-#    load_dotenv()
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vote_app_backend.settings')
     try:
         from django.core.management import execute_from_command_line
