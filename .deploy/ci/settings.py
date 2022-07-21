39399e415721152e00b5c9dfb5d0f070fd962957
JaninaAlona <jelona@posteo.net>
2022-07-21 11:08:10 +0200

    settings BE reduced

--- .deploy/ci/settings.py
+++ .deploy/ci/settings.py
@@ -119,20 +119,10 @@ ALLOWED_HOSTS = [
     str(os.environ.get('DOMAIN_URL')),
 ]
 
-
-CORS_ALLOWED_ORIGINS = [
-    "http://" + str(os.environ.get('DOMAIN_URL')),
-]
-
 CSRF_TRUSTED_ORIGINS = [
     "http://" + str(os.environ.get('DOMAIN_URL')),
 ]
 
-CORS_ALLOW_METHODS = [
-    'GET',
-    'POST',
-    'OPTIONS',
-]
 
 
 CORS_ALLOW_CREDENTIALS = True
@@ -159,13 +149,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-CSRF_COOKIE_SECURE = True
-CSRF_COOKIE_SAMESITE = 'Strict'
-#allow specific subdomain
-#CSRF_COOKIE_DOMAIN = "www." + str(os.environ.get('DOMAIN_URL'))
-#allow all subdomains:
-#CSRF_COOKIE_DOMAIN = "." + str(os.environ.get('DOMAIN_URL'))
-SESSION_COOKIE_SECURE = True
-SESSION_COOKIE_SAMESITE = 'Strict'
