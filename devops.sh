7ab5da52c6ad7d051c238f156fca359fa0cec687
JaninaAlona <jelona@posteo.net>
2022-07-29 16:53:02 +0200

    devops script adjusted

--- devops.sh
+++ devops.sh
@@ -17,16 +17,12 @@ function install() {
       cp .deploy/${LOC}/vue.config.js frontend-ui/vue.config.js || true
       cp .deploy/${LOC}/settings.py vote_app_backend/vote_app_backend/settings.py || true
       COPIED=true
-    else
-      echo "wrong execution folder specified, specify local, stage or ci"
-      ABORT=true
-    fi
-  fi
-  if [ "$LOC" == "ci" ];then
-    if [ ! -f docker-compose.yml ] && [ ! -f vote_app_backend/vote_app_backend/settings.py ];then
-      cp .deploy/${LOC}/docker-compose.yml docker-compose.yml || true
-      cp .deploy/${LOC}/settings.py vote_app_backend/vote_app_backend/settings.py || true
-      COPIED=true
+    elif [ "$LOC" == "ci" ];then
+      if [ ! -f docker-compose.yml ] && [ ! -f vote_app_backend/vote_app_backend/settings.py ];then
+        cp .deploy/${LOC}/docker-compose.yml docker-compose.yml || true
+        cp .deploy/${LOC}/settings.py vote_app_backend/vote_app_backend/settings.py || true
+        COPIED=true
+      fi
     else
       echo "wrong execution folder specified, specify local, stage or ci"
       ABORT=true
