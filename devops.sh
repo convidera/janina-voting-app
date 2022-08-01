8d9ba871ee9bd11959baa8dbbb3cebaf803427da
JaninaAlona <jelona@posteo.net>
2022-08-01 16:26:35 +0200

    devops script op LOC added

--- devops.sh
+++ devops.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 set -euo pipefail
 COMPOSE="docker-compose"
-copied=0
 export LOC=${LOC:-local}
 
 function install() {
   if [ "$LOC" == "local" ];then
     docker network create proxy
     if [ ! -f docker-compose.yml ] && [ ! -f frontend-ui/vue.config.js ] && [ ! -f vote_app_backend/vote_app_backend/settings.py ];then
-      echo "hello"
       cp .deploy/${LOC}/docker-compose.yml docker-compose.yml || true
       cp .deploy/${LOC}/vue.config.js frontend-ui/vue.config.js || true
       cp .deploy/${LOC}/settings.py vote_app_backend/vote_app_backend/settings.py || true
-      copied=1
     fi
   fi
   if [ "$LOC" == "ci" ];then
     if [ ! -f docker-compose.yml ] && [ ! -f vote_app_backend/vote_app_backend/settings.py ];then
       cp .deploy/${LOC}/docker-compose.yml docker-compose.yml || true
       cp .deploy/${LOC}/settings.py vote_app_backend/vote_app_backend/settings.py || true
-      copied=1
     fi
   fi
   if [ "$LOC" == "stage" ];then
@@ -27,7 +23,6 @@ function install() {
       cp .deploy/${LOC}/docker-compose.yml docker-compose.yml || true
       cp .deploy/${LOC}/vue.config.js frontend-ui/vue.config.js || true
       cp .deploy/${LOC}/settings.py vote_app_backend/vote_app_backend/settings.py || true
-      copied=1
     fi
   fi
 }
@@ -46,108 +41,76 @@ function cleanUp() {
 
 if [ $# -gt 0 ]
 then
-  if [ "$1" == "runserver" ];then
+  if [ "$LOC" == "local" ] || [ "$LOC" == "ci" ] || [ "$LOC" == "stage" ];then
     install
-    if [ $copied -eq 1 ];then
-      shift 1
-      $COMPOSE run --rm \
-        backend-part \
-        gunicorn vote_app_backend.wsgi:application "$@"
-      cleanUp
-    fi
-  elif [ "$1" == "migrate" ];then
-    install
-    if [ $copied -eq 1 ];then
+    if [ "$1" == "migrate" ];then
       $COMPOSE run --rm \
         backend-part \
         python manage.py migrate
-      cleanUp
-    fi
-  elif [ "$1" == "makemigrations" ];then
-    install
-        if [ $copied -eq 1 ];then
+    elif [ "$1" == "makemigrations" ];then
       $COMPOSE run --rm \
         backend-part \
         python manage.py makemigrations showvotes
-      cleanUp
-    fi
-  elif [ "$1" == "flush" ];then
-    install
-    if [ $copied -eq 1 ];then
+    elif [ "$1" == "flush" ];then
       $COMPOSE run --rm \
         backend-part \
         python manage.py flush
-      cleanUp
-    fi
-  elif [ "$1" == "npm" ]; then
-    install
-    if [ $copied -eq 1 ];then
-      shift 1
-      $COMPOSE run --rm \
-        frontend-part \
-        npm run serve "$@"
-      cleanUp
-    fi
-  elif [ "$1" == "test" ];then
-    install
-    if [ $copied -eq 1 ];then
+    elif [ "$1" == "test" ];then
       shift 1
       $COMPOSE run --rm \
         backend-part \
         pytest "$@"
-      cleanUp
-    fi
-  elif [ "$1" == "push" ];then 
-    git add * && \
-    git commit -m "$2" && \
-    shift 2
-    git push "$@"
-  elif [ "$1" == "pullserver" ];then
-    #check if .env exists
-    if [ -f .env ]; then
-      #import environment variables from .env
-      export $(cat .env | xargs)
-      #check if GITHUB_USER and GITHUB_TOKEN strings exist in .env
-      if grep -Fq GITHUB_USER .env && grep -Fq GITHUB_TOKEN .env
-      then
-        #check if variables are unset
-        if [ -z "$GITHUB_USER" ] && [ -z "$GITHUB_TOKEN" ];then
-          echo "environment variables unset in .env file"
-        else
-          ./pullserverbot.exp $GITHUB_USER $GITHUB_TOKEN
-        fi
-      else
-        echo "environment variables missing in .env file"
-      fi
     else
-      echo ".env file missing"
-    fi
-  elif [ "$1" == "sshserver" ];then
-    if [ -f .env ]; then
-      export $(cat .env | xargs)
-      if grep -Fq SERVER_USER .env && grep -Fq SERVER_IP .env && grep -Fq SERVER_PASSPHRASE .env
-      then
-        if [ -z "$SERVER_USER" ] && [ -z "$SERVER_IP" ] && [ -z "$SERVER_PASSPHRASE" ];then
-          echo "environment variables unset in .env file"
-        else
-          ./sshserverbot.exp $SERVER_USER $SERVER_IP $SERVER_PASSPHRASE
-        fi
-      else
-        echo "environment variables missing in .env file"
-      fi
-    else
-      echo ".env file missing"
-    fi
-  else
-    install
-    if [ $copied -eq 1 ];then
       $COMPOSE "$@"
-      cleanUp
-    fi 
+    fi
+    cleanUp
+  else [ "$LOC" == "op" ];then
+    if [ "$1" == "push" ];then 
+      git add * && \
+      git commit -m "$2" && \
+      shift 2
+      git push "$@"
+    elif [ "$1" == "pullserver" ];then
+      #check if .env exists
+      if [ -f .env ]; then
+        #import environment variables from .env
+        export $(cat .env | xargs)
+        #check if GITHUB_USER and GITHUB_TOKEN strings exist in .env
+        if grep -Fq GITHUB_USER .env && grep -Fq GITHUB_TOKEN .env
+        then
+          #check if variables are unset
+          if [ -z "$GITHUB_USER" ] && [ -z "$GITHUB_TOKEN" ];then
+            echo "environment variables unset in .env file"
+          else
+            ./pullserverbot.exp $GITHUB_USER $GITHUB_TOKEN
+          fi
+        else
+          echo "environment variables missing in .env file"
+        fi
+      else
+        echo ".env file missing"
+      fi
+    elif [ "$1" == "sshserver" ];then
+      if [ -f .env ]; then
+        export $(cat .env | xargs)
+        if grep -Fq SERVER_USER .env && grep -Fq SERVER_IP .env && grep -Fq SERVER_PASSPHRASE .env
+        then
+          if [ -z "$SERVER_USER" ] && [ -z "$SERVER_IP" ] && [ -z "$SERVER_PASSPHRASE" ];then
+            echo "environment variables unset in .env file"
+          else
+            ./sshserverbot.exp $SERVER_USER $SERVER_IP $SERVER_PASSPHRASE
+          fi
+        else
+          echo "environment variables missing in .env file"
+        fi
+      else
+        echo ".env file missing"
+      fi
+    fi
   fi
 else
-  install
-  if [ $copied -eq 1 ];then
+  if [ "$LOC" == "local" ];then
+    install
     $COMPOSE up
     cleanUp
   fi
