c324a661d4d753f84f8b70ef5a9557c941b571d3
JaninaAlona <jelona@posteo.net>
2022-08-24 16:38:10 +0200

    port must be a number error

--- devops.sh
+++ devops.sh
@@ -50,7 +50,8 @@ then
                 else
                     $COMPOSE -f "${upfile}" exec -T \
                         backend-part \
-                        bash -c "until nc -z -v -w30 vote-app-mysql 3306; do sleep 2; done;"
+                        bash -c "until nc -z -v -w30 "$MYSQL_HOST" "$MYSQL_PORT"; do sleep 2; done;"
+#                        bash -c "until nc -z -v -w30 vote-app-mysql 3306; do sleep 2; done;"
                 fi
             fi
         fi
